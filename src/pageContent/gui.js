fbdddc2da51aff2ef1e661ca95e548a605775ef9
Paul Bergamo <porlybe@gmail.com>
2019-05-02 17:52:00 +0800

    Style GUI page

---
+++ src/pageContent/gui.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    title: 'BIP44 development wallet',
+    subtitle:
+      'Create as many BIP44 accounts as you want on mainnet or testnet. See their address, WIF, XPub and XPriv.',
+    link: '/gui/docs/wallet',
+  },
+  {
+    title: 'Address Conversion',
+    subtitle:
+      'Paste in legacy, cashaddr, wif, xpub or xpriv and get address conversion and QR codes.',
+    link: '/gui/docs/conversion',
+  },
+  {
+    title: 'Sign/Verify messages',
+    subtitle:
+      'Sign messages in legacy or cashaddr with any address in your GUI. Verify messages in legacy or cashaddr from any wallet.',
+    link: '/gui/docs/sign-verify',
+  },
+  {
+    title: 'Configured how you like it',
+    subtitle:
+      'Create as many accounts as you need. Vary the entropy in the mnemonic. Supports 8 languages. Custom HD Paths. Add a password. Mainnet or Testnet.',
+    link: '/gui/docs/configuration',
+  },
+];
