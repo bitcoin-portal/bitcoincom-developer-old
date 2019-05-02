4e1fd92c0a9fae426cb22a56b92837cc9dfaadc9
Paul Bergamo <porlybe@gmail.com>
2019-05-02 16:57:19 +0800

    Style badger page

--- src/pages/badger.js
+++ src/pages/badger.js
@@ -6,69 +6,51 @@ import { graphql } from 'gatsby';
 
 import DefaultLayout from 'components/layouts/DefaultLayout';
 import HelmetPlus from 'components/HelmetPlus';
-import Hero from 'components/Hero';
-import Container from 'components/Container';
 
-import StyledLink, { SmartLink } from 'atoms/StyledLink';
-import Text from 'atoms/Text';
-import H3 from 'atoms/H3';
-import H1 from 'atoms/H1';
-import Button from 'atoms/Button';
+import {
+  theme,
+  media,
+  ContentBlock,
+  H1,
+  H2,
+  H3,
+  Button,
+  Card,
+} from 'bitcoincom-storybook';
 
-import media from 'styles/media';
-import spacing from 'styles/spacing';
-
-const HeroLayout = styled.div`
-  display: grid;
-  grid-gap: ${spacing.tiny};
+const StyledContentBlock = styled(ContentBlock)`
+  & > div > :nth-child(2) {
+    flex-basis: 75%;
+  }
+  & > div > div > img {
+    display: none;
+  }
 `;
 
-const InstallCTA = styled.div`
-  margin-top: ${spacing.medium};
+const ButtonWrapper = styled.div`
   display: grid;
-  grid-auto-flow: dense;
-  grid-template-columns: max-content;
-  grid-gap: ${spacing.small};
+  grid-column-gap: ${theme.spacing.unit * 2}px;
+  grid-row-gap: ${theme.spacing.unit * 2}px;
+  margin: 0 auto;
+  justify-items: center;
+  ${media.md`
+    grid-template-columns: 1fr 1fr 1fr;
+  `}
 `;
-
-const PreviewLayout = styled.div`
+const CardContainer = styled.div`
   display: grid;
-  padding-top: ${spacing.large};
-  grid-gap: ${spacing.medium};
-  grid-template-columns: 1fr;
-  ${media.medium`
-    grid-template-columns: repeat(auto-fit, minmax(300px, .5fr));
-  `};
+  grid-row-gap: ${theme.spacing.unit * 4}px;
+  grid-column-gap: ${theme.spacing.unit * 4}px;
+  margin-top: ${theme.spacing.unit * 8}px;
+  ${media.md`
+    grid-template-columns: 1fr 1fr;
+  `}
+
+  & > div {
+    margin: 0 auto;
+    background-color: ${theme.palette.background.default};
+  }
 `;
-
-const ItemLayout = styled.div`
-  display: grid;
-  grid-gap: ${spacing.tiny};
-  grid-auto-rows: max-content;
-  grid-column: span 2;
-  ${media.medium`
-    grid-column: span 1;
-    grid-template-rows: max-content max-content 1fr;
-  `};
-`;
-
-type ItemProps = {
-  children: React.Node,
-  to?: string,
-};
-
-const PreviewItem = ({ children, to }: ItemProps) => (
-  <ItemLayout>
-    {children}
-    {to && (
-      <div style={{ display: 'flex', alignItems: 'flex-end' }}>
-        <StyledLink to={to}>
-          <Button round>View</Button>
-        </StyledLink>
-      </div>
-    )}
-  </ItemLayout>
-);
 
 type Props = {
   location: Object,
@@ -83,7 +65,27 @@ type Props = {
 };
 
 const BadgerPage = ({ location, data }: Props) => (
-  <DefaultLayout location={location}>
+  <DefaultLayout
+    location={location}
+    heroImage={data.heroImage}
+    hero={
+      <StyledContentBlock image="none">
+        <H3 style={{ color: theme.palette.primary.main }}>
+          The easiest way to build BCH apps
+        </H3>
+        <H1 contrast>Badger SDK</H1>
+        <H2 contrast>Your gateway to the world of Bitcoin Cash (BCH) apps</H2>
+        <ButtonWrapper>
+          <Button round link href="https://badger.bitcoin.com" target="_blank">
+            Install Badger
+          </Button>
+          <Button round link dark href="/badger/docs/getting-started">
+            Start Here
+          </Button>
+        </ButtonWrapper>
+      </StyledContentBlock>
+    }
+  >
     <HelmetPlus
       title={`Badger - ${data.site.siteMetadata.title}`}
       description="Badger documentation, your gateway to the world of Bitcoin Cash (BCH) applications"
@@ -96,42 +98,21 @@ const BadgerPage = ({ location, data }: Props) => (
       ]}
       location={location}
     />
-    <Hero image={data.heroImage}>
-      <HeroLayout>
-        <H3 primary thin>
-          The easiest way to build BCH apps
-        </H3>
-        <H1 background>Badger SDK</H1>
-        <H3 background thin>
-          Your gateway to the world of Bitcoin Cash (BCH) apps
-        </H3>
-        <InstallCTA>
-          <SmartLink to="https://badger.bitcoin.com">Install Badger</SmartLink>
-          <StyledLink to="/badger/docs/getting-started">
-            <Button round>Start Here</Button>
-          </StyledLink>
-        </InstallCTA>
-      </HeroLayout>
-    </Hero>
-    <Container>
-      <PreviewLayout>
-        <PreviewItem to="/badger/docs/getting-started">
-          <H3>Badger SDK</H3>
-          <Text>
-            Badger Wallet injects an API into pages a user visits to allow apps
-            to request a users's permission to send Bitcoin Cash, send tokens,
-            or authenticate with CashID.
-          </Text>
-        </PreviewItem>
-        <PreviewItem to="/badger/docs/badger-components-react">
-          <H3>Badger React Components</H3>
-          <Text>
-            React based components and tools to make integrating Bitcoin Cash
-            (BCH) into your next project easy.
-          </Text>
-        </PreviewItem>
-      </PreviewLayout>
-    </Container>
+    <ContentBlock>
+      <CardContainer>
+        <Card
+          title="Badger SDK"
+          subtitle="Badger Wallet injects an API into pages a user visits to allow apps to request a users's permission to send Bitcoin Cash, send tokens, or authenticate with CashID."
+          cta={{ text: 'View', link: '/badger/docs/getting-started' }}
+        />
+
+        <Card
+          title="Badger React Components"
+          subtitle="React based components and tools to make integrating Bitcoin Cash (BCH) into your next project easy."
+          cta={{ text: 'View', link: '/badger/docs/badger-components-react' }}
+        />
+      </CardContainer>
+    </ContentBlock>
   </DefaultLayout>
 );
 
