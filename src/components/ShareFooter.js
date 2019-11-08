b50bd0c455f94e0d1796377764f7e9ba581186cb
ncribt <ncribt@icloud.com>
2019-11-08 10:01:39 +0900

    remove buttons and title for optinmonster

--- src/components/ShareFooter.js
+++ src/components/ShareFooter.js
@@ -1,15 +1,7 @@
 // @flow
 
 import React from 'react';
-import styled from 'styled-components';
 import { Helmet } from 'react-helmet';
-import spacing from 'styles/spacing';
-import { ContentBlock, H2, Button } from 'bitcoincom-storybook';
-
-const EmailCTA = styled.div`
-  display: grid;
-  grid-gap: ${spacing.small};
-`;
 
 const SharedFooter = () => (
   <>
@@ -22,20 +14,7 @@ const SharedFooter = () => (
         async
       />
     </Helmet>
-    <ContentBlock>
-      <H2>Do You Want To Get Free Developer Resources?</H2>
-      <EmailCTA>
-        <div id="om-v8lwzo6nqacmgnulutqp-holder" />
-      </EmailCTA>
-      <Button
-        link
-        primary
-        href="https://app.monstercampaigns.com/c/v8lwzo6nqacmgnulutqp/"
-        style={{ margin: 'auto', marginTop: 32 }}
-      >
-        {"Yes, I'm Interested"}
-      </Button>
-    </ContentBlock>
+    <div id="om-v8lwzo6nqacmgnulutqp-holder" />
   </>
 );
 
